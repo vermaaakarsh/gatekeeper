125f9e4efd0826599c9f04928a7811a0878dd776
Aakarsh Verma <vaakarshofficial@gmail.com>
2026-02-14 10:37:38 +0530

    Modified api docs and updated infra to manage runtime configs

--- infra/terraform/modules/alb/main.tf
+++ infra/terraform/modules/alb/main.tf
@@ -41,13 +41,35 @@ resource "aws_lb_target_group" "this" {
 }
 
 # ----------------------------------------
-# Listener (HTTP)
+# HTTP Listener (Redirect to HTTPS)
 # ----------------------------------------
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# ----------------------------------------
+# HTTPS Listener
+# ----------------------------------------
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
 
   default_action {
     type             = "forward"
